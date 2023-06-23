4747a1803aea42acc669a207e83ca509af3713dd
Furky <77547158+coderfeye13@users.noreply.github.com>
2023-06-23 13:46:31 +0300

    Update PasswordLengthInput.java

--- src/PasswordLengthInput.java
+++ src/PasswordLengthInput.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class PasswordLengthInput {
+import java.util.Scanner;
+
+public class PasswordLengthInput {
+    private int uzunluk;
+
+    public void kullaniciGirisiniAl() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Şifre uzunluğunu girin: ");
+        uzunluk = scanner.nextInt();
+    }
+
+    public int getUzunluk() {
+        return uzunluk;
+    }
 }
